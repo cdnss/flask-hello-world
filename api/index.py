8c5c2e6a21094bf921fab5b4860de7423fbda3c8
cdnss <32522595+cdnss@users.noreply.github.com>
2024-11-14 15:34:47 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,18 +7,26 @@ import sys
 import socket
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask,jsonify,request
-import json
+from flask import Flask,jsonify,request,Response
+import json, html
 app = Flask(__name__)
 
 
-target = "https://anoboy.icu"
+target = "https://154.26.137.28"
 
 def geturl(x):
-    res = requests.get(x)
+    cookies = {
+    cookie['name']: cookie['value'] for cookie in [
+        {"domain": "154.26.137.28", "expirationDate": 1731597407, "hostOnly": True, "httpOnly": False, "name": "_as_ipin_tz", "path": "/", "sameSite": "Strict", "secure": False, "session": False, "storeId": "0", "value": "Asia/Jakarta"},
+        {"domain": "154.26.137.28", "expirationDate": 1731597407, "hostOnly": True, "httpOnly": False, "name": "_as_ipin_lc", "path": "/", "sameSite": "Strict", "secure": False, "session": False, "storeId": "0", "value": "id"},
+        {"domain": "154.26.137.28", "expirationDate": 1731597407, "hostOnly": True, "httpOnly": False, "name": "_as_ipin_ct", "path": "/", "sameSite": "Strict", "secure": False, "session": False, "storeId": "0", "value": "ID"}
+    ]
+}
+
+    res = requests.get(x, cookies=cookies)
     return res.text
 
-def rex(tag_name, attribute_name, a, soup, host):
+def rex(tag_name, attribute_name, a, soup, host, css):
     if a != 1:
         for tag in soup.find_all(tag_name):
             if tag.get(attribute_name) and not tag.get(attribute_name).startswith("http"):
@@ -28,59 +36,111 @@ def rex(tag_name, attribute_name, a, soup, host):
             qq = tag.get(attribute_name)
             # Check if qq is not None before calling replace
             if qq is not None and not tag.get(attribute_name).startswith("http"):
-                
-                tag[attribute_name] = host + tag.get(attribute_name)
-
+                if css != 1:
+                  tag[attribute_name] = host + tag.get(attribute_name)
+                else:
+                  tag[attribute_name] = host +"/f"+ tag.get(attribute_name) +"?"
             else:
-                tag[attribute_name] = qq.replace(target, host)
+                if tag.get(attribute_name):
+                  if css != 1:
+                    tag[attribute_name] = qq.replace(target, host)
+                  else:
+                    tag[attribute_name] = qq.replace(target, host+"/f") + "?"
                 
 def head(soup, host):
-    elements = soup.select("script[type='application/ld+json'], div[id^='ad'], #judi, #judi2, #disqus_thread, .sidebar, #coloma")
+    elements = soup.select("script[type='application/ld+json'], #judi, #judi2, #disqus_thread, .sidebar, #as_radio-js, script[id^=disqus] ")
     if elements:  # Check if any elements were selected
         for element in elements:
             element.decompose() #use decompose instead of clear
-    rex("link", "href", "", soup, host)
-    rex("script", "src", "", soup, host)
-    rex("amp-img", "src", "", soup, host)
-    rex("img", "src", "", soup, host)
-    rex("a", "href", "", soup, host)
-    rex("a", "href", 1, soup, host)
-    rex("iframe", "src", "", soup, host)
-    html = soup.prettify().replace("</head>", """
-  <style>
-  #menu,   div.column-three-fourth  { width:100% !important;
-           overflow: hidden;
-          }
+    rex("link", "href", 1, soup, host, 1)
+    #rex("script", "src", 1, soup, host, 1)
+    rex("amp-img", "src", "", soup, host, "")
+    rex("img", "src", "", soup, host, "")
+    rex("a", "href", "", soup, host, "")
+    rex("a", "href", 1, soup, host, "")
+    rex("iframe", "src", "", soup, host, "")
+    html = soup.prettify().replace("$(document).find('#pembed').html(atob(defaultpembed));", """
 
-   
-  </style>
-  </head>
+$(document).find('#pembed').html(atob(defaultpembed));
+}
+  var sa = jQuery("#embed_holder iframe").attr("src");
+  if ( sa.startsWith('"""+target+"""') ) {
+  
+  var sb = sa.replace('"""+target+"""', '')
+  
+  jQuery("#pembed").html(`
+  
+  <iframe src="${sb}" frameborder="0" marginwidth="0" marginheight="0" scrolling="NO" width="100%" height="100%" allowfullscreen="true"></iframe>
+  
+  
+  `)
+  
+  alert( sb );
+
+  """).replace("function statistic()", "/* ").replace('jQuery("#pembed").html(embed);', F"""
+  
+  jQuery("#pembed").html(embed);
+  
+  var sc = jQuery("#embed_holder iframe").attr("src");
+  if ( sc.startsWith('"""+target+"""') ) {
+  
+  var sv = sc.replace('"""+target+"""', '/')
+  
+  jQuery("#pembed").html(`
+  
+  <iframe src="${sv}" frameborder="0" marginwidth="0" marginheight="0" scrolling="NO" width="100%" height="100%" allowfullscreen="true"></iframe>
+  
+  
+  `)
+  
+  
+  }
+  
   """)
+   
     return html
 
 
 @app.route("/")
 def ok():
-    host = "http://" +request.host + "/post"
+    host = "http://" +request.host
     soup = BeautifulSoup(geturl(target), 'html.parser')
     print( request.host )
     return head(soup, host)
 
-@app.route("/post/<path:all>" , strict_slashes=False)
+@app.route("/<path:all>" , strict_slashes=False)
 def post(all):
-    host = "http://"+request.host + "/post"
-    url = request.path.split("/post")[1]
+    host = "http://"+request.host
+    url = request.full_path
     soup = BeautifulSoup(geturl(target+url), 'html.parser')
-    print( request.host )
+    print( target+url )
     return head(soup, host)
     
-@app.route("/post/")
-def pot():
-    host = "http://" +request.host + "/post"
-    soup = BeautifulSoup(geturl(target), 'html.parser')
-    print( request.host )
+@app.route("/f/<path:i>", strict_slashes=False)
+def f(i):
+    host = "http://" +request.host
+    url = request.full_path.split("/f")[1]
+    soup = BeautifulSoup(geturl(target+url), 'html.parser')
+    res = ""
+    if url.endswith("css?"):
+      res = Response(head(soup, host), mimetype='text/css')
+    elif url.endswith(".js?"):
+      res = Response(head(soup, host), mimetype='text/json')
+    elif url.endswith("xml?"):
+      res = Response(head(soup, host), mimetype='text/xml')  
+    else:
+      res = head(soup, host)
+      
+    return res
+    """
+@app.route("/anime/<path:a>" , strict_slashes=False)
+def anime(a):
+    host = "http://"+request.host + "/post"
+    url = request.full_path
+    soup = BeautifulSoup(geturl(target+url), 'html.parser')
+    print( target+url )
     return head(soup, host)
-
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
